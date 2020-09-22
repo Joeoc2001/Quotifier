0691c444fc7b6e9c68f2fe6d0b729e365e796441
Joeoc2001 <joeoc2625@gmail.com>
2020-09-22 20:40:30 +0100

    Started to add image processing logic

--- src/main/java/dev/joeoc/quotifier/Bot.java
+++ src/main/java/dev/joeoc/quotifier/Bot.java
@@ -3,13 +3,26 @@ package dev.joeoc.quotifier;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.utils.AttachmentOption;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 public class Bot extends ListenerAdapter {
+    private final Font font;
+
+    public Bot() throws URISyntaxException, IOException, FontFormatException {
+        font = Font.createFont(Font.TRUETYPE_FONT, new File(new URI("https://fonts.google.com/download?family=Rogue%20Script")));
+    }
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         Message msg = event.getMessage();
@@ -37,56 +50,39 @@ public class Bot extends ListenerAdapter {
 
         MessageChannel channel = event.getChannel();
 
-        if (messageParts.length == 1) {
+        if (messageParts.length <= 2) {
             channel.sendMessage("[INFO]").queue();
             return;
         }
 
-        Guild guild = msg.getGuild();
-
         String name = messageParts[1];
+        name = name.replace("_", " ");
 
-        List<Member> members = msg.getMentionedMembers();
+        BufferedImage image = getBufferedImage(100, 100);
 
-        channel.sendMessage("Mentioned: " + members.stream().map(Member::getNickname).collect(Collectors.joining(", "))).queue();
+        Graphics2D g = image.createGraphics();
+        g.setFont(font);
+        g.drawString(name, 50, 50);
 
-        channel.sendMessage("Quote from " + name).queue();
-
-        Member attributation = getMemberByName(guild, name);
-        channel.sendMessage("User " + attributation).queue();
-
-        if (attributation != null) {
-            channel.sendMessage("Most recent message: " + getMostRecentMessageByUser(channel, attributation.getUser(), 100)).queue();
+        InputStream file;
+        try {
+            file = getStreamFromBuffer(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
         }
+
+        channel.sendFile(file, "quote.jpg").queue();
     }
 
-    public static Member getMemberByName(Guild guild, String name) {
-        Member possibleMember = guild.getMemberById(name);
-        if (possibleMember != null) {
-            return possibleMember;
-        }
+    public static BufferedImage getBufferedImage(int width, int height) {
+        return new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    }
 
-        possibleMember = guild.getMemberByTag(name);
-        if (possibleMember != null) {
-            return possibleMember;
-        }
-
-        List<Member> possibleMembers = guild.getMembersByEffectiveName(name, true);
-        if (possibleMembers.size() == 1) {
-            return possibleMembers.get(0);
-        }
-
-        possibleMembers = guild.getMembersByName(name, true);
-        if (possibleMembers.size() == 1) {
-            return possibleMembers.get(0);
-        }
-
-        possibleMembers = guild.getMembersByNickname(name, true);
-        if (possibleMembers.size() == 1) {
-            return possibleMembers.get(0);
-        }
-
-        return null;
+    public static InputStream getStreamFromBuffer(BufferedImage bufferedImage) throws IOException {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "jpg", outStream);
+        return new ByteArrayInputStream(outStream.toByteArray());
     }
 
     public static CompletableFuture<Optional<Message>> getMostRecentMessageByUser(MessageChannel channel, User user, int depth) {
